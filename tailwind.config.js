867be1b1c4932674df71320a6b4f1514779850ac
HI-SHADOW <dev.gautamvaja@gmail.com>
2024-04-22 23:28:49 +0530

    :art: edit tailwind config file for small devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,13 @@ export default {
   theme: {
     extend: {},
     screens: {
-      xs: "320px",
-      // => @media (min-width: 320px) {... }
+      xss: "320px",
+      xs: "422px",
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
   darkMode: "class",
